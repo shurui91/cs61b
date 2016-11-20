62f0fd89188db3f0199796e7fb62febf726f6252
Shurui Liu <shurui91@gmail.com>
2016-11-19 22:34:28 -0800

    hw0/2 bug fixed

--- hw0/2.java
+++ hw0/2.java
@@ -1,18 +1,17 @@
 /* Exercise 2 */
 public class Solution {
 	public static int max(int[] m) {
-	int a = m[0];
-	for (int i = 1; i < m.length; i++) {
-		if (a < m[i]) {
-		a = m[i];
+		int a = m[0];
+		for (int i = 1; i < m.length; i++) {
+			if (a < m[i]) {
+			a = m[i];
+			}
 		}
-	}
-	System.out.println(a);
-	return 0;
+		return a;
 	}
 	
 	public static void main(String[] args) {
-	int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
-	max(numbers);
+		int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+		System.out.println(max(numbers));
 	}
 }
