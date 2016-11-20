758019f5a91068cd843d24acb3e2866227e3afda
Shurui Liu <shurui91@gmail.com>
2016-11-19 22:32:46 -0800

    hw0/2 is done

--- hw0/2.java
+++ hw0/2.java
@@ -1,14 +1,18 @@
 /* Exercise 2 */
 public class Solution {
 	public static int max(int[] m) {
-		Array sorted = Array.sort(m);
-		int length = m.length;
-		int max_value = m[length - 1];
-		System.out.println(max_value);
+	int a = m[0];
+	for (int i = 1; i < m.length; i++) {
+		if (a < m[i]) {
+		a = m[i];
+		}
 	}
-
+	System.out.println(a);
+	return 0;
+	}
+	
 	public static void main(String[] args) {
-		int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
-		max(numbers);
+	int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+	max(numbers);
 	}
 }
