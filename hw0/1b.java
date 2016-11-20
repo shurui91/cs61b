af3ccf6299e4cb7f4d07d922fa0e66a01c66e79a
Shurui Liu <shurui91@gmail.com>
2016-11-19 21:44:40 -0800

    1b code is updated

--- hw0/1b.java
+++ hw0/1b.java
@@ -1,14 +1,14 @@
 /* Creative Exercise 1a: Drawing a Triangle */
 public class Solution {
-    public static void drawTriangle(int N) {
-        String s = "*";
-        for (int i = 1; i < N; i++) {
-            String repeated = new String(new char[i]).replace("\0", s);
-            System.out.print(repeated + "\n");
-        }
-    }
+	public static void drawTriangle(int N) {
+		String s = "*";
+		for (int i = 1; i < N; i++) {
+			String repeated = new String(new char[i]).replace("\0", s);
+			System.out.print(repeated + "\n");
+		}
+	}
 
-    public static void main(String[] args) {
-        drawTriangle(10);
-    }
+	public static void main(String[] args) {
+		drawTriangle(10);
+	}
 }
